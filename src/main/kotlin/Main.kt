fea1625b21e505feb292b915d3d768a08f50c707
Kim-Yukyung <yukyung116@gmail.com>
2025-10-23 00:19:49 +0900

    ✨ Study control flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -64,4 +64,59 @@ fun main() {
     juice["cherry"] = 200
     juice.remove("kiwi")
     println("juice = $juice\n")
+
+    // if
+    val a = 1
+    val b = 2
+    println("The larger number is ${if (a > b) a else b}\n")
+
+    // when
+    val obj = "Hello"
+//    when (obj) {
+//        "1" -> println("One")
+//        "Hello" -> println("Greeting")
+//        else -> println("Unknown")
+//    }
+    val result = when (obj) {
+        "1" -> "One"
+        "Hello" -> "Greeting"
+        else -> "Unknown"
+    }
+    println("Result: $result")
+
+    val trafficLight = "Red"
+    val action = when {
+        trafficLight == "Green" -> "Go"
+        trafficLight == "Yellow" -> "Slow down"
+        trafficLight == "Red" -> "Stop"
+        else -> "Malfunction"
+    }
+    println("Traffic light action: $action\n")
+
+    // range
+    println(1..5)
+    println("'z' downTo 's' → ${('z' downTo 's').joinToString()}\n")
+
+    // for loop
+    for (i in 1..5) {
+        print(i)
+    }
+    println()
+    println()
+
+    // while & do-while
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    while (cakesEaten < 3) {
+        println("Eat a cake: $cakesEaten")
+        cakesEaten++
+    }
+    println()
+
+    do {
+        println("Bake a cake: $cakesBaked")
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+    println()
 }
